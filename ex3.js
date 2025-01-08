c50dd082aea948905672803e9a992afed54eaaa0
Rubens <rubenskarvalhoveloso@gmail.com>
2025-01-08 00:54:54 -0300

    Exércicio 3

---
+++ ex3.js
@@ -0,0 +1,9 @@
+let numero1 = Number (prompt ("Digte o primeiro número:"))
+
+let numero2 = Number (prompt ("Digite o segundo número:"))
+
+let numero3 = Number (prompt ("Digite o terceiro número:"))
+
+let media = (numero1 + numero2 + numero3) / 3
+
+console.log ("A média é:" + media)
