295bfdbbcc57342e3e6b951b687dfe07f03f1d86
Rubens <rubenskarvalhoveloso@gmail.com>
2025-01-09 18:50:05 -0300

    Exercício 5

---
+++ ex5.js
@@ -0,0 +1,11 @@
+function calculaMediaDe (num1, num2, num3, media){
+num1 = Number (prompt ("Digite o primeiro número:"));
+
+num2 = Number (prompt ("Digite o segundo número:"));
+
+num3 = Number (prompt ("Digite o terceiro número:"));
+
+media = (num1 + num2 + num3) / 3;
+console.log ("Média:" + media);
+}
+calculaMediaDe ();
