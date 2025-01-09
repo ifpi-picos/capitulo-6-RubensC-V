a3fc5d613d41ded2dada243eb463c468e96e2671
Rubens <rubenskarvalhoveloso@gmail.com>
2025-01-09 19:13:25 -0300

    Exércicio 6

---
+++ ex6.js
@@ -0,0 +1,6 @@
+function calculaQuadradoDe (numero){
+    numero = Number (prompt ("Digite um número:"));
+    return (numero ** 2);
+}
+let resultado = calculaQuadradoDe ();
+console.log (resultado)
