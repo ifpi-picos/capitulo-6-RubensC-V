fa33707aaee709ca5d417db6e76787cc1dc58a0a
Rubens <rubenskarvalhoveloso@gmail.com>
2025-01-11 17:10:20 -0300

    Execício 10

---
+++ ex10.js
@@ -0,0 +1,8 @@
+const calculaMediaDe = (num1, num2) => {
+
+    num1 = Number (prompt ("Digite o primeiro número:"));
+    num2 = Number (prompt ("Digite o segundo número:"));
+
+    return (num1 + num2) / 2;
+} 
+console.log ("MÉDIA:", calculaMediaDe());
